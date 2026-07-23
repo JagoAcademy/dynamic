670e62bd987495bb39328626058c541a53d7cbed
JagoAcademy <academyjagorenang@gmail.com>
2026-07-23 10:57:21 +0700

    parent html

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,12 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     rollupOptions: {
-      // Di sini kita daftarin semua halaman HTML lo biar ikut ke-deploy
       input: {
         main: 'index.html',
         login: 'login.html',
         coach: 'coach.html',
-        siswa: 'siswa.html'
-        // Kalau nanti lo bikin file event.html atau training.html, tambahin juga di sini ya
+        siswa: 'siswa.html',
+        parent: 'parent.html' // <-- Tambahin ini
       }
     }
   }
